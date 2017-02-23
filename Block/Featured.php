3d56a2cd7b654091e06526ef0e657f91d6630976
Bolot <bolotkalil@gmail.com>
2017-02-23 21:43:53 +0600

    do not visible featured block if is empty items

--- Block/Featured.php
+++ Block/Featured.php
@@ -8,18 +8,23 @@ class Featured extends \Phpfox_Component
 {
     public function process()
     {
+        $aDDs = \Phpfox::getService('digitaldownload.browse')
+            ->conditions([
+                'AND `d`.`featured` = 1',
+                ' AND `d`.`is_active` = 1'
+            ])
+            ->limit(\Phpfox::getParam('cm_dd_feature_limit', 4))
+            ->page(1)
+            ->sort(' RAND()')
+            ->getCollection(false);
+        if (!(count($aDDs) > 0)) {
+            return 'block';
+        }
+
         $this->template()
             ->assign([
                 'sHeader' => _p('Featured'),
-                'aDDs' =>  \Phpfox::getService('digitaldownload.browse')
-                    ->conditions([
-                        'AND `d`.`featured` = 1',
-                        ' AND `d`.`is_active` = 1'
-                    ])
-                    ->limit(\Phpfox::getParam('cm_dd_feature_limit', 4))
-                    ->page(1)
-                    ->sort(' RAND()')
-                    ->getCollection(false),
+                'aDDs' =>  $aDDs,
             ]);
 
         return 'block';
