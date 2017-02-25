0bd2a7130262484cbdb6474eb400ab629d845198
Bolot <bolotkalil@gmail.com>
2017-02-25 12:54:32 +0600

    re #15 fixed design of apply options

--- views/controller/apply-options.html.php
+++ views/controller/apply-options.html.php
@@ -6,28 +6,31 @@ defined('PHPFOX') or exit('NO DICE!');
     {module name='api.gateway.form'}
 {else}
     <h3>{_p('Options you\'re buying')}</h3>
-    <form method="post">
-        <input type="hidden" name="dd_id" value="{$oDD.id}">
-        <input type="hidden" name="confirm_pay" value="1">
-        <input type="hidden" name="process" value="1">
-        <article class="cm-dd-search-item">
-            <div class="cm-dd-search-item-img">
-                <a href="{$oDD.url}">
-                    {img path='core.url_pic' file=$oDD.main_image.image_path server_id=$oDD.main_image.server_id suffix='_120_square' itemprop='image'}
-                </a>
-            </div>
-            <div class="cm-dd-search-item-content">
-                <p><a href="{$oDD.url}">{$oDD}</a></p>
-                {foreach from=$aOptions key=sOptionName item=aItem}
+    <div class="cm-dd-view-block list">
+        <form method="post" class="cd-dd dd-box-product-outer">
+            <input type="hidden" name="dd_id" value="{$oDD.id}">
+            <input type="hidden" name="confirm_pay" value="1">
+            <input type="hidden" name="process" value="1">
+            <article class="cm-dd-search-item dd-box-product row">
+                <div class="col-sm-3">
+                    <a href="{$oDD.url}">
+                        <span class="dd-product-img"
+                              style="background-image: url({img path='core.url_pic' file=$oDD.main_image.image_path server_id=$oDD.main_image.server_id suffix='_200_square' return_url=true});"
+                              itemprop='image'></span>
+                    </a>
+                </div>
+                <div class="col-sm-9">
+                    <p><a href="{$oDD.url}">{$oDD}</a></p>
+                    {foreach from=$aOptions key=sOptionName item=aItem}
                     <input type="hidden" name="options[paid][{$sOptionName}]" value="1">
                     <p>
-                        <div class="info">
-                            <div class="info_left">{$aItem.caption}</div>
-                            <div class="info_right">{$sPlanCurrencyId|currency_symbol}{$aItem.price}</div>
-                        </div>
+                    <div class="info">
+                        <div class="info_left">{$aItem.caption}</div>
+                        <div class="info_right">{$sPlanCurrencyId|currency_symbol}{$aItem.price}</div>
+                    </div>
                     </p>
-                {/foreach}
-                <p>
+                    {/foreach}
+                    <p>
                     <div class="info">
                         <div class="info_left">
                             <strong>{_p('Total')}</strong>
@@ -36,10 +39,13 @@ defined('PHPFOX') or exit('NO DICE!');
                             <strong>{$sPlanCurrencyId|currency_symbol}{$iTotalPrice}</strong>
                         </div>
                     </div>
-                </p>
-            </div>
-        </article>
-        <p>{_p('By clicking on the button below')}</p>
-        <input type="submit" value="{_p('Commit to Buy')}"  class="button btn-primary"/>
-    </form>
+                    </p>
+                    <p>{_p('By clicking on the button below')}</p>
+                    <input type="submit" value="{_p('Commit to Buy')}"  class="button btn-primary"/>
+                    <p></p>
+                </div>
+            </article>
+        </form>
+    </div>
+
 {/if}
